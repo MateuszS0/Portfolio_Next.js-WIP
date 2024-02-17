f3b7ef076d5a687f1d207930e2a8534e46bf9064
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2024-02-17 16:49:53 +0100

    style: color

--- portfolio_nextjs/components/Header/header.tsx
+++ portfolio_nextjs/components/Header/header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
     <div className="header-wrapper flex justify-center mt-20">
         <div className="w-full flex gap-4">
             <div className="w-4/6 m-6">
-                <h1 className="flex justify-center text-4xl " >Mateusz Stolarczyk</h1>
-                <h2 className="flex justify-center text-lg">Web Developer & Software engineer</h2>
+                <h1 className="flex justify-center text-5xl text-decoration-primary" >Mateusz Stolarczyk</h1>
+                <h2 className="flex justify-center text-2xl bg-gradient-to-r from-blue-700 to-green-300 text-transparent bg-clip-text">Web Developer & Software engineer</h2>
                 <div className="flex justify-center mx-20">I'm 22 and really interested in programming and learning new things. Started with basic HTML and a bit of CSS at 15 just for fun. Now im trying to create more complex websites using different technologies. </div>
             </div>
             <div className="w-2/6 m-8"><Animation/></div>
