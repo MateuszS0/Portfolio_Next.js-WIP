e1b7084efbbb361012931082a4691c5bf7f8b727
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2024-02-17 17:28:50 +0100

    style: colors and buttons

--- portfolio_nextjs/components/Header/header.tsx
+++ portfolio_nextjs/components/Header/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
             <div className="w-4/6 m-6">
                 <h1 className="flex justify-center text-5xl text-decoration-primary" >Mateusz Stolarczyk</h1>
                 <h2 className="flex justify-center text-2xl bg-gradient-to-r from-blue-700 to-green-300 text-transparent bg-clip-text">Web Developer & Software engineer</h2>
-                <div className="flex justify-center mx-20">I'm 22 and really interested in programming and learning new things. Started with basic HTML and a bit of CSS at 15 just for fun. Now im trying to create more complex websites using different technologies. </div>
+                <div className="flex justify-center mx-20 text-lg">I'm 22 and really interested in programming and learning new things. Started with basic HTML and a bit of CSS at 15 just for fun. Now im trying to create more complex websites using different technologies. </div>
             </div>
             <div className="w-2/6 m-8"><Animation/></div>
         </div>
--- portfolio_nextjs/components/Navbar/navbar.tsx
+++ portfolio_nextjs/components/Navbar/navbar.tsx
@@ -1,8 +1,12 @@
 
-
 const Navbar = () => {
     return(
-        <div className="navbar-wrapper">Navbar</div>
+        <div className="navbar-wrapper flex justify-center gap-8">
+            <div className="button">About</div>
+            <div className="button">Skills</div>
+            <div className="button">Projects</div>
+            <div className="button">Contact</div>
+        </div>
     )
 }
 export default Navbar;
