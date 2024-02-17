d415ed1c2f4bf4df7f043723ac78c7d55f171d69
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2024-02-17 16:38:35 +0100

    feat: text

--- portfolio_nextjs/components/Header/header.tsx
+++ portfolio_nextjs/components/Header/header.tsx
@@ -2,9 +2,13 @@ import Animation from "../Animation/animation";
 
 const Header = () => {
     return(
-    <div className="header-wrapper flex justify-center">
+    <div className="header-wrapper flex justify-center mt-20">
         <div className="w-full flex gap-4">
-            <div className="w-4/6 m-6">Etiam quis dui feugiat, euismod enim eu, ullamcorper orci. In lobortis quis quam et maximus. Sed rhoncus, lacus vitae consectetur laoreet, turpis tellus bibendum lectus, ut rutrum est nunc sit amet lacus. Nunc nisi mauris, gravida vel luctus in, egestas ut ligula. Curabitur rutrum, leo ac placerat fermentum, lectus diam rutrum libero, eu fermentum dui libero vitae elit. Nunc ornare ac elit non facilisis. Praesent justo ex, aliquam eu vestibulum vitae, aliquet ac sapien. Integer mollis neque mauris, non consectetur libero porta eget. Fusce molestie turpis non velit dignissim, id vehicula mauris vehicula. Aliquam accumsan maximus orci vitae dapibus. Proin volutpat, erat et varius tristique, diam elit maximus augue, vitae dignissim urna magna pulvinar diam. Praesent rhoncus tortor sodales, aliquam lacus et, posuere nisi.</div>
+            <div className="w-4/6 m-6">
+                <h1 className="flex justify-center text-4xl " >Mateusz Stolarczyk</h1>
+                <h2 className="flex justify-center text-lg">Web Developer & Software engineer</h2>
+                <div className="flex justify-center mx-20">I'm 22 and really interested in programming and learning new things. Started with basic HTML and a bit of CSS at 15 just for fun. Now im trying to create more complex websites using different technologies. </div>
+            </div>
             <div className="w-2/6 m-8"><Animation/></div>
         </div>
     </div>
