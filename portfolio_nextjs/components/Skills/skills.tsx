a460a109973bbee792f35f1fffbf69cb59f97119
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2024-02-20 18:42:05 +0100

    chore: ideas

--- portfolio_nextjs/components/Skills/skills.tsx
+++ portfolio_nextjs/components/Skills/skills.tsx
@@ -3,6 +3,7 @@ const Skills = () => {
         <div className="skills-wrapper flex justify-center">Skills</div>
         //make blocks "frontend" "backend" "other" which you can click
         //once clicked you see the specific skills
+        //the skills go in a slide? from top right and when clicked back they go to top left
     )
 }
 export default Skills;
