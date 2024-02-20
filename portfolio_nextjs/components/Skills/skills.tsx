b6f6edefcb0dcb627432731e808a25b64fe702c1
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2024-02-20 17:52:33 +0100

    feat: radial-gradient

--- portfolio_nextjs/components/Skills/skills.tsx
+++ portfolio_nextjs/components/Skills/skills.tsx
@@ -1,6 +1,8 @@
 const Skills = () => {
     return(
         <div className="skills-wrapper flex justify-center">Skills</div>
+        //make blocks "frontend" "backend" "other" which you can click
+        //once clicked you see the specific skills
     )
 }
 export default Skills;
