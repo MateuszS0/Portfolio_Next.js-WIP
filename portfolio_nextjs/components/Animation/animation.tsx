955d87a9d6d2d8a46758da1d017603c31b7da0b5
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2024-02-22 15:13:30 +0100

    feat: p5.js animation

--- portfolio_nextjs/components/Animation/animation.tsx
+++ portfolio_nextjs/components/Animation/animation.tsx
@@ -1,6 +1,19 @@
-const Animation = () => {
-    return(
-        <div className="animation-wrapper flex justify-center">Animation</div>
-    )
-}
+"use client"
+
+const Animation  = () => {
+  return (
+    <div className="wrapper w-full h-full">
+        <div className="">
+        <img
+        src="P5-Animation.gif"
+        width={500}
+        height={500}
+        alt="GIF"
+        loading="lazy"
+        />
+        </div>
+    </div>
+  );
+};
+
 export default Animation;
