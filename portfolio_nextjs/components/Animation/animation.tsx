403cc950b5ce663304ce7e7df5246ffa8c7a2fa7
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2024-02-24 16:28:14 +0100

    feat: skills structure

--- portfolio_nextjs/components/Animation/animation.tsx
+++ portfolio_nextjs/components/Animation/animation.tsx
@@ -4,6 +4,7 @@ const Animation  = () => {
   return (
     <div className="wrapper w-full h-full">
         <div className="">
+          <a href="https://github.com/MateuszS0/Random/tree/main/p5-project">
         <img
         src="P5-Animation.gif"
         width={500}
@@ -11,6 +12,7 @@ const Animation  = () => {
         alt="GIF"
         loading="lazy"
         />
+        </a>
         </div> <small className="flex justify-center">Made in p5.js</small>
         {/* maybe make it a hover over text ? */}
     </div>
--- portfolio_nextjs/components/Skills/skills.tsx
+++ portfolio_nextjs/components/Skills/skills.tsx
@@ -1,6 +1,30 @@
 const Skills = () => {
     return(
-        <div className="skills-wrapper flex justify-center">Skills</div>
+        <div className="skills-wrapper">
+            <h2 className="flex justify-center text-4xl">Skills</h2>
+            <div className=" w-full flex justify-center">
+            <div className="w-1/4 flex justify-center">
+                <div className="text-2xl">Web Development
+                <img 
+                src="" 
+                alt="" 
+                />
+                </div>
+            </div>
+            <div className="w-1/4 flex justify-center">
+            <div className="text-2xl">Other
+                <img 
+                src="" 
+                alt="" 
+                />
+                </div>
+            </div>
+            </div>
+
+        
+        
+        
+        </div>
         //make blocks "frontend" "backend" "other" which you can click
         //once clicked you see the specific skills
         //the skills go in a slide? from top right and when clicked back they go to top left
