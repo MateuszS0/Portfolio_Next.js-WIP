1bef80f79c8894e1a047e7bc016f15e2f8c5f6e8
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2024-02-24 16:45:36 +0100

    style: photo, styling

--- portfolio_nextjs/components/Skills/skills.tsx
+++ portfolio_nextjs/components/Skills/skills.tsx
@@ -2,23 +2,24 @@ const Skills = () => {
     return(
         <div className="skills-wrapper">
             <h2 className="flex justify-center text-4xl">Skills</h2>
-            <div className=" w-full flex justify-center">
-            <div className="w-1/4 flex justify-center">
-                <div className="text-2xl">Web Development
-                <img 
-                src="" 
-                alt="" 
-                />
+            <div className=" w-full flex justify-center ">
+                <div className="w-1/4 flex justify-center p-6">
+                    <div className="text-2xl text-center">Web Development
+                    <img className="button my-6"
+                    src="/web-dev-photo.jpg" 
+                    alt="Web Dev" 
+                    loading="lazy"
+                    />
+                    </div>
                 </div>
-            </div>
-            <div className="w-1/4 flex justify-center">
-            <div className="text-2xl">Other
-                <img 
-                src="" 
-                alt="" 
-                />
+                <div className="w-1/4 flex justify-center p-6">
+                    <div className="text-2xl text-center">Other
+                    <img className="button my-6"
+                    src="/web-dev-photo.jpg" 
+                    alt="Other" 
+                    />
+                    </div>
                 </div>
-            </div>
             </div>
 
         
