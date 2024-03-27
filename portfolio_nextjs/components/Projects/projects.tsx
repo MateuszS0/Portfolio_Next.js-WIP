e7f83fa4b96808c64b0049c0d4f659106dcd2102
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2024-03-27 18:13:29 +0100

    style: projects and pictures

--- portfolio_nextjs/components/Projects/projects.tsx
+++ portfolio_nextjs/components/Projects/projects.tsx
@@ -4,26 +4,36 @@ const Projects = () => {
     return(
         <>
         <h2 className="flex justify-center mt-40 text-4xl">Projects</h2>
-        <div className="projects-wrapper flex justify-center">
+        {/* make UI for selecting projects */}
+        {/* <div className="projects-wrapper flex justify-center">
             <div className="project">
-                <a href="https://github.com/MateuszS0/PriceAlert">
-                    <img src="otherSkills.jpg"
-                    alt="PriceAlert" 
+                <a href="https://github.com/MateuszS0/Map">
+                    <img className="project-image"
+                    width={300}
+                    loading="lazy"
+                    src="projects/map.jpg" 
+                    alt="Map" 
                     />
-                    <h2 className="project-title">PriceAlert: placeholder</h2>
+                    <h2 className="project-title">Map</h2>
                 </a>
             </div>
             <div className="project">
-                <a href="https://github.com/MateuszS0/Map">
-                    <img src="otherSkills.jpg" 
-                    alt="Map" 
+                <a href="https://github.com/MateuszS0/React-Portfolio">
+                    <img className="project-image"
+                    width={300}
+                    loading="lazy"
+                    src="projects/portfolio-picture.png" 
+                    alt="React-portfolio" 
                     />
-                    <h2 className="project-title">Map: placeholder</h2>
+                    <h2 className="project-title">Portfolio V1</h2>
                 </a>
             </div>
             <div className="project">
                 <a href="https://github.com/MateuszS0/Random/tree/main/p5-project">
-                    <img src="P5-Animation.gif" 
+                    <img className="project-image"
+                    width={340}
+                    loading="lazy"
+                    src="projects/p5.jpg" 
                     alt="FlowField" 
                     />
                     <h2 className="project-title">Flow field</h2>
@@ -31,21 +41,27 @@ const Projects = () => {
             </div>
             <div className="project">
                 <a href="https://github.com/MateuszS0/Webflix">
-                    <img src="otherSkills.jpg" 
+                    <img className="project-image"
+                    width={500}
+                    loading="lazy"
+                    src="projects/webflix.png" 
                     alt="Webflix" 
                     />
-                    <h2 className="project-title">Webflix: placeholder</h2>
+                    <h2 className="project-title">Webflix</h2>
                 </a>
             </div>
             <div className="project">
-                <a href="https://github.com/MateuszS0/React-Portfolio">
-                    <img src="otherSkills.jpg" 
-                    alt="React-portfolio" 
+                <a href="https://github.com/MateuszS0/PriceAlert">
+                    <img className="project-image"
+                    width={340}
+                    loading="lazy"
+                    src="/projects/priceAlert.jpg"
+                    alt="PriceAlert" 
                     />
-                    <h2 className="project-title">Portfolio V1: placeholder</h2>
+                    <h2 className="project-title">PriceAlert</h2>
                 </a>
             </div>
-        </div>
+        </div> */}
         </>
     )
 }
